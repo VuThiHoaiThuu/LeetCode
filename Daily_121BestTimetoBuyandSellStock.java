20ae864502a6e563009daee1f14434bf40b181d5
VuThiHoaiThuu <vuthihoaithu031003@gmail.com>
2023-01-16 21:29:38 +0700

    Daily LeedCode

---
+++ Daily_121BestTimetoBuyandSellStock.java
@@ -0,0 +1,21 @@
+public class Daily_121BestTimetoBuyandSellStock {
+    public static void main(String[] args) {
+        int[] prices = {2,4,1};
+        int[] prices1 = {7,6,4,3,1};
+        System.out.println(maxProfit(prices));
+    }
+    public static int maxProfit(int[] prices){
+        if (prices == null || prices.length < 2)
+            return 0;
+        int min = prices[0];
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            if (prices[i] - min > max)
+                max = prices[i] - min;
+        }
+        return max;
+    }
+}
